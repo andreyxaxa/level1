e0c0b6e70ab49d2cefc1025abbbdb670fb62856d
Petrov Andrey <4ndrew1337@mail.ru>
2025-09-11 01:39:22 +0300

    l1.9

---
+++ 9/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+// 1. Пишем в переданный канал все элементы массива
+func generate(ch chan<- int, numbers []int) {
+	go func() {
+		for _, v := range numbers {
+			ch <- v
+		}
+		close(ch)
+	}()
+}
+
+// 2. В 'chWrite' будет записан каждый 'v*2' из 'chRead'
+func multiply(chRead <-chan int, chWrite chan<- int) {
+	go func() {
+		for v := range chRead {
+			chWrite <- v * 2
+		}
+		close(chWrite)
+	}()
+}
+
+func main() {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+
+	numbers := []int{10, 20, 30, 40, 50, 60, 70, 80, 90, 100}
+
+	generate(ch1, numbers)
+	multiply(ch1, ch2)
+
+	// Выводим данные
+	for v := range ch2 {
+		fmt.Println(v)
+	}
+}
