e69872c3e7c1a76a9f43e9a38d541938effea996
Petrov Andrey <4ndrew1337@mail.ru>
2025-09-16 00:57:35 +0300

    l1.15

--- 15/main.go
+++ 15/main.go
@@ -1,10 +1,12 @@
 package main
 
+import "strings"
+
 var justString string
 
 func someFunc() {
 	v := createHugeString(1 << 10)
-	justString = v[:100]
+	justString = strings.Clone(v[:100]) // копируем
 }
 
 func main() {
