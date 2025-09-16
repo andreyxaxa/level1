3df7697354490437b8aebd450d6c9492f5ced7f1
Petrov Andrey <4ndrew1337@mail.ru>
2025-09-17 01:14:11 +0300

    l1.18

---
+++ 18/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+// Atomic
+type AtomicCounter struct {
+	value atomic.Int64
+}
+
+func (c *AtomicCounter) inc() {
+	c.value.Add(1)
+}
+
+// Mutex
+type MutexCounter struct {
+	value int
+	mu    sync.Mutex
+}
+
+func (c *MutexCounter) inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.value++
+}
+
+// go run main.go -race
+func main() {
+	wg := sync.WaitGroup{}
+
+	// atomic
+	ac := &AtomicCounter{}
+
+	for i := 0; i < 50; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for j := 0; j < 100; j++ {
+				ac.inc()
+			}
+		}()
+	}
+
+	// mutex
+	mc := &MutexCounter{}
+
+	for i := 0; i < 50; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for j := 0; j < 100; j++ {
+				mc.inc()
+			}
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println(ac.value.Load())
+	fmt.Println(mc.value)
+}
