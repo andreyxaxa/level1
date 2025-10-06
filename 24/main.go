40f396dcf440ec1cd658923b87f86b993d925341
Petrov Andrey <4ndrew1337@mail.ru>
2025-10-06 17:48:09 +0300

    l1.24

---
+++ 24/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Point struct {
+	x float64
+	y float64
+}
+
+func NewPoint(x, y float64) *Point {
+	return &Point{x, y}
+}
+
+func (p *Point) Distance(other Point) float64 {
+	return math.Sqrt(math.Pow(other.x-p.x, 2) + math.Pow(other.y-p.y, 2))
+}
+
+func main() {
+	p1 := NewPoint(3, 3)
+	p2 := NewPoint(6, 5)
+
+	fmt.Println(p1.Distance(*p2))
+}
